a4e489a58dd4408f7fbe475fc934f71537f20aaf
gianlucafiore <gfiorepersonal@gmail.com>
2024-12-31 00:54:52 -0300

    subo

--- common.js
+++ common.js
@@ -15,18 +15,18 @@ const calcSpanProps = (spanMonths, spanDays) =>
           { unit: "week", format: "[Sem.]w", offset: spanDays > 200 ? 2 : 1 },
         ],
       }
-    : spanDays > 26
-    ? {
-        columnOffset: 3,
-        columnUnit: "day",
-        magnetUnit: "day",
-        magnetOffset: 1,
-        alignStartTo: "week",
-        headers: [
-          { unit: "week", format: "MM/YYYY", offset: 1 },
-          { unit: "day", format: "DD", offset: Math.ceil(spanDays / 50) },
-        ],
-      }
+    // : spanDays > 26
+    // ? {
+    //     columnOffset: 3,
+    //     columnUnit: "day",
+    //     magnetUnit: "day",
+    //     magnetOffset: 1,
+    //     alignStartTo: "week",
+    //     headers: [
+    //       { unit: "week", format: "MM/YYYY", offset: 1 },
+    //       { unit: "day", format: "DD", offset: Math.ceil(spanDays / 50) },
+    //     ],
+    //   }
     : spanDays > 20
     ? {
         columnOffset: 1,
@@ -37,7 +37,7 @@ const calcSpanProps = (spanMonths, spanDays) =>
         headers: [
           { unit: "week", format: "MM/YYYY", offset: 1 },
 
-          { unit: "day", format: "dd DD", offset: 1 },
+          { unit: "day", format: "d DD", offset: 1 },
         ],
       }
     : spanDays > 8
